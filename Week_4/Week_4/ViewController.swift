52d829b1e4fddddaa728b63aa52c744ccdbcc467
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-07-18 23:22:54 +0900

    [Week4] first init
    setting secene delegate

---
+++ Week_4/Week_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week_4
+//
+//  Created by 김상준 on 7/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
